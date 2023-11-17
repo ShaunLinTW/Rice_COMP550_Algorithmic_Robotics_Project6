4955feef1b6f80325d5d018616197bf4231ec65a
HsuanYouLin <yoyomy7198come@gmail.com>
2023-11-17 15:44:56 -0600

    update RVO no_obstacles/with_obstacles results

--- src/Project6.py
+++ src/Project6.py
@@ -8,7 +8,6 @@
 
 import sys
 
-
 from RVO import RVO_update, reach, compute_V_des, reach
 from vis import visualize_traj_dynamic
 
@@ -20,9 +19,9 @@ ws_model = dict()
 ws_model['robot_radius'] = 0.2
 #circular obstacles, format [x,y,rad]
 # no obstacles
-ws_model['circular_obstacles'] = []
+# ws_model['circular_obstacles'] = []
 # with obstacles
-# ws_model['circular_obstacles'] = [[-0.3, 2.5, 0.3], [1.5, 2.5, 0.3], [3.3, 2.5, 0.3], [5.1, 2.5, 0.3]]
+ws_model['circular_obstacles'] = [[-0.3, 2.5, 0.3], [1.5, 2.5, 0.3], [3.3, 2.5, 0.3], [5.1, 2.5, 0.3]]
 #rectangular boundary, format [x,y,width/2,heigth/2]
 ws_model['boundary'] = [] 
 
@@ -59,7 +58,7 @@ while t*step < total_time:
     #----------------------------------------
     # visualization
     if t%10 == 0:
-        visualize_traj_dynamic(ws_model, X, V, goal, time=t*step, name='../visualization/snap%s.png'%str(t/10))
-        #visualize_traj_dynamic(ws_model, X, V, goal, time=t*step, name='../visualization/snap%s.png'%str(t/10))
+        # visualize_traj_dynamic(ws_model, X, V, goal, time=t*step, name='../visualization/no_obstacles/snap%s.png'%str(t/10))
+        visualize_traj_dynamic(ws_model, X, V, goal, time=t*step, name='../visualization/with_obstacles/snap%s.png'%str(t/10))
     t += 1
     
---
+++ visualization/with_obstacles/mkmovie.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Set the input and output file names
+INPUT_PATTERN="snap%d.0.png"
+OUTPUT_FILE="Project6_with_obstacles.gif"
+
+ffmpeg -y -i $INPUT_PATTERN -vf "fps=30,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $OUTPUT_FILE
